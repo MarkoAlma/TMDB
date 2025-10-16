f5a2556379331b5672c2be00cbea15299e42723d
MarkoAlma <markogyapjas@gmail.com>
2025-10-16 21:02:32 +0200

    1006 third commit

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -21,7 +21,7 @@ data && console.log(data);
     <Slider {...settings} style={{width:'100%'}}>
       {data && data.cast.map(obj => 
         <div key={obj.id} style={{display:'flex', alignItems:'center', justifyContent:'center', textAlign:'center'}}>
-          <img style={{width:'60px', margin:'auto'}} src={obj.profile_path ? img_300+obj.profile_path : no_image} alt={obj?.name} />
+          <img style={{width:'60px', margin:'auto', borderRadius:'0'}} src={obj.profile_path ? img_300+obj.profile_path : no_image} alt={obj?.name} />
           <b style={{textAlign:'center', width:'100%'}}>{obj?.name}</b>
           
         </div>
